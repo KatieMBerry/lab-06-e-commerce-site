3c64a64bb9835ebfcfb1f792171900bb32bea530
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-05 15:30:03 -0700

    added product.css, product.html, and data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+export const cakes = [
+    {
+        id: 'carrot-cake22',
+        name: 'Carrot Cake',
+        image: 'carrot-cake.png',
+        price: 15.99, 
+        description: 'Low-fat, gluten-free and high protein.  Whassup Doc?',
+        category: 'low-fat-cakes',
+        onSale: true,
+    }
+
+];
---
+++ utils.js
@@ -0,0 +1,6 @@
+export function renderBakedGood {
+    const li = document.querySelector();
+
+
+    
+}
