edbc8bf6be7b32f9f27b66bcaa72f9fbc1868e92
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-07 15:17:34 -0700

    fixed bugs from yesterday's work

--- cart/cart.js
+++ cart/cart.js
@@ -1,31 +1,32 @@
-import { cart, foodItems } from '../data.js';
+import { foodItems } from '../data.js';
 import { renderTableRow } from './cart-utils.js';
 import { findById } from '../utils.js';
+import { cart } from '../data/static-cart.js';
 
 const table = document.querySelector('tbody');
 
 for (let i = 0; i < cart.length; i++) {
-    const foodItem = cart[i];
-
-    const tr = renderTableRow(foodItem);
-
+    const cartItem = cart[i];
+    
+    const tr = renderTableRow(cartItem, foodItems);
+    
     table.appendChild(tr);
 }
 
-const total = calculateTotal(cart);
+const total = calculateTotal(cart, foodItems);
 
 const totalCell = document.querySelector('.total');
 
 totalCell.textContent = `Total: $${total}`;
 
-function calculateTotal(cartArray) {
+function calculateTotal(cartArray, foodItems) {
     let grandTotal = 0;
 
     for (let i = 0; i < cartArray.length; i++) {
         const cartItem = cartArray[i];
         const item = findById(foodItems, cartItem.id);
 
-        const subTotal = item.price * item.quantity;
+        const subTotal = item.price * cartItem.quantity;
 
         grandTotal += subTotal;
     }
--- test/cart-utils.test.js
+++ test/cart-utils.test.js
@@ -8,11 +8,11 @@ const test = QUnit.test;
 test('should take in a cartItem and return a tr ele with the appropriate contents', (expect) => {
     // arrange
     const cartItem = {
-        id: 'carrot-cake22',
-        price: 15.99, 
+        id: 'carrot-cake',
+        quantity: 2, 
     };
     
-    const expected = '<tr><td>Apple</td><td>$1.50</td><td>3</td><td>$4.50</td><tr>';
+    const expected = '<tr><td>Low-fat Carrot Cake</td><td>$15.99</td><td>2</td><td>$31.98</td></tr>';
 
     // act
     const actual = renderTableRow(cartItem);
@@ -24,7 +24,7 @@ test('should take in a cartItem and return a tr ele with the appropriate content
 test('should take in cart line totals and return a tr ele grand total value', (expect) => {
     const cart = {
         id: 'carrot-cake',
-        quantity: 2
+        price: 15.99
     };
 
     const expected = '<tr><td>Total: </td><td>$31.98</td><tr>';
