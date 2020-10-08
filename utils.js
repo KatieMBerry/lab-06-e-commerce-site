4d4f8729161d8684140f621c87633b96ea96491e
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-07 20:20:58 -0700

    added product to cart, added place order button with functionality

--- utils.js
+++ utils.js
@@ -1,11 +1,13 @@
 
+export const CART = 'CART';
+
 export function findById(someArray, someId) {
     for (let i = 0; i < someArray.length; i++) {
         const item = someArray[i];
         if (item.id === someId) {
             return item;
-        }
-    } return null;
+        } 
+    } 
 }
 
 export function renderFoodItem(foodItem) {
@@ -15,6 +17,7 @@ export function renderFoodItem(foodItem) {
     const image = document.createElement('img');
     const price = document.createElement('p');
     const button = document.createElement('button');
+    const removeButton = document.createElement('button');
 
     li.classList.add('food-items');
     title.classList.add('name');
@@ -22,6 +25,7 @@ export function renderFoodItem(foodItem) {
     image.classList.add('food-image');
     price.classList.add('item-price');
     button.classList.add('add-button');
+    removeButton.classList.add('remove-item-button');
     
     title.textContent = foodItem.name;
     li.appendChild(title);
@@ -36,8 +40,32 @@ export function renderFoodItem(foodItem) {
     li.appendChild(price);
 
     button.textContent = 'Add to Cart';
+    button.addEventListener('click', () => {
+        alert('You chose ' + foodItem.name + ' for your cart!');
+        const cart = getFromLocalStorage(CART) || [];
+        const itemInCart = findById(cart, foodItem.id);
+        
+        if (itemInCart === undefined) {
+            const newCartItem = {
+                id: foodItem.id,
+                quantity: 1
+            };
+            cart.push(newCartItem);
+        } else {
+            itemInCart.quantity++;
+        }
+        
+        setInLocalStorage(CART, cart);
+    });
+    removeButton.textContent = 'Remove One';
+    removeButton.addEventListener('click', () => {
+        const cart = getFromLocalStorage(CART);
+        const itemInCart = findById(cart, foodItem.id);
+        itemInCart.quantity--;
+    });
+
     li.appendChild(button);
-    // const img = document.createElement('image');
+    li.appendChild(removeButton);
 
     return li;
 }
@@ -45,4 +73,19 @@ export function renderFoodItem(foodItem) {
 export function calcLineItem(quantity, amount) {
     const lineItem = quantity * amount;
     return lineItem;
+}
+
+export function getFromLocalStorage(key) {
+    const item = localStorage.getItem(key);
+
+    return JSON.parse(item);
+}
+
+// this function will not return anything
+export function setInLocalStorage(key, value) {
+    const stringyItem = JSON.stringify(value);
+
+    localStorage.setItem(key, stringyItem);
+
+    return value;
 }
