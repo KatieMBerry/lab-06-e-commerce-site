4fe7abd47b93f006ecfaf4f9e1c29280e7a4c29f
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-08 21:05:43 -0700

    added seedandgetproducts, addFood function, tried testing

--- products/products.js
+++ products/products.js
@@ -1,27 +1,11 @@
-import { foodItems as hardCodedFoods } from '../data.js';
-import { renderFoodItem } from '../utils.js';
+import { renderFoodItem, seedAndGetProducts } from '../utils.js';
 
-const PRODUCTS = 'PRODUCTS';
-
-// go get localStorageBooks from local storage and call them localStorageBooks (if there are some there already)
-let localStorageFoods = JSON.parse(localStorage.getItem(PRODUCTS));
-
-// if there are no localStorageBooks in local storage yet
-    // if they've never been to the site
-if (!localStorageFoods) {
-     // go grab the hard coded localStorageBooks, and SEED local storage with them.  First we'll need to stringify those:
-    const stringyFoods = JSON.stringify(hardCodedFoods);
-
-    localStorage.setItem(PRODUCTS, stringyFoods);
-    //Using hardCodedFoods to SET/SEED to local storage & then we can loop through them
-    localStorageFoods = hardCodedFoods;
-    
-}
+const localStorageFoods = seedAndGetProducts();
 
 const ul = document.querySelector('#list');
 
-for (let i = 0; i < hardCodedFoods.length; i++) {
-    const item = hardCodedFoods[i];
+for (let i = 0; i < localStorageFoods.length; i++) {
+    const item = localStorageFoods[i];
 
     const li = renderFoodItem(item);
 
--- test/cart-utils.test.js
+++ test/cart-utils.test.js
@@ -1,6 +1,7 @@
 // IMPORT MODULES under test here:
 import { calcOrderTotal } from '../cart/cart.js';
 import { renderTableRow } from '../cart/cart-utils.js';
+import { seedAndGetProducts, addFood } from '../utils.js';
 
 
 const test = QUnit.test;
@@ -35,10 +36,77 @@ test('should take in cart line totals and return a tr ele grand total value', (e
     expect.equal(actual.outerHTML, expected);
 });
 
-// test('should take in a cart\'s line item totals and return a tr ele that is the total cost of items in the cart', (expect) => {
-//     const cart = {
-//         id: 'carrot-cake',
-//         quantity: 2
-//     };
-//     const expected = 
-// });
+test('addProduct should take in a product object and add it to local storage (returning nothing)', (expect) => {
+    const newFood = {
+        id: 'salad420',
+        name: 'Chickpea Salad',
+        image: 'chick-pea.png',
+        price: 12.50,
+        description: 'Delicious and Healthy!',
+        category: 'salad',
+    };
+
+    const expected = [
+        {
+            id: 'carrot-cake',
+            name: 'Low-fat Carrot Cake',
+            image: 'carrot-cake.jpeg',
+            price: 15.99, 
+            description: 'Low-fat, gluten-free and high protein.  Whassup Doc?',
+            category: 'low-fat-cakes',
+        },
+        {
+            id: 'charcuterie',
+            name: 'Charcuterie',
+            image: 'charcuterie-plate.PNG',
+            price: 18.99, 
+            description: 'Assorted meats, cheeses, fruits & veg.  Perfect for sharing.',
+            category: 'shares',
+        },
+        {
+            id: 'shortbread',
+            name: 'Million Dollar Shortbread',
+            image: 'shortbread.PNG',
+            price: 12.99, 
+            description: 'Rich and decadent.',
+            category: 'cookies',
+        },
+        {
+            id: 'torte',
+            name: 'Chocolate Torte',
+            image: 'torte.PNG',
+            price: 20.99, 
+            description: 'Dense and silky.  Great with coffee!',
+            category: 'confections',
+        },
+        {
+            id: 'assorted-cookies',
+            name: 'Assorted Cookies',
+            image: 'assorted-cookies.jpg',
+            price: 14.99, 
+            description: 'Assorted meats, cheeses, fruits & veg.  Perfect for sharing.',
+            category: 'gifts',
+        },
+        {
+            id: 'croissant',
+            name: 'Croissants',
+            image: 'croissant.jpeg',
+            price: 10.99, 
+            description: 'Pillowy!',
+            category: 'breads',
+        },
+        {
+            id: 'salad420',
+            name: 'Chickpea Salad',
+            image: 'chick-pea.png',
+            price: 12.50,
+            description: 'Delicious and Healthy!',
+            category: 'salad',
+        }
+    ];
+
+    addFood(newFood);
+
+    const actual = seedAndGetProducts(expected);
+    expect.deepEqual(actual, expected);
+});
